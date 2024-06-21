6109b2299ebb884f7eac006e0487a39fa0425096
KiranPandit-del <kiran.pandit@onepointltd.com>
2024-06-21 13:43:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ session = cnx.session()
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)     
+st.text(fruityvice_response.json())     
 
 
 
