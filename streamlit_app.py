0069cabccbc5d2372ee93d9096f98bfa3bafd15f
KiranPandit-del <kiran.pandit@onepointltd.com>
2024-06-21 14:00:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,22 @@
-#import python packages
+# Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-st.title('My Parents New Healty Diner')
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smothie!:cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
     """)
+
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:',name_on_order)
+
 session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe ,
@@ -24,23 +29,28 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    L= st.write(ingredients_string)   
+    L= st.write(ingredients_string)
+
+    
 
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert :
-         #session.sql(my_insert_stmt).collect()  
+         #session.sql(my_insert_stmt).collect()
+    
 
          st.success('Your Smoothie is ordered ,'+ name_on_order+'!', icon="✅")
 
+      
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json()) 
-fv_df = st.dataframe(data = fruityvice_response.json(),use_container_width = True)
+#st.text(fruityvice_response.json())
+fv_dt = st.dataframe(data = fruityvice_response.json(),use_container_width=True)
+
+
 
 
 
